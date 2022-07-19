664228d0d32150d0b1c5664f76e6bc127c869f31
Shivang Gupta <shivangguptaxd@gmail.com>
2022-07-19 22:11:34 +0530

    initial commit

--- Typical Questions/1.1 )  Jump Game/5 (Jump Game V).cpp
+++ Typical Questions/1.1 )  Jump Game/5 (Jump Game V).cpp
@@ -8,9 +8,11 @@ public:
         //right
         for(int j = i+1 ; j <= min(i+d,n-1) && arr[i] > arr[j] ; j++)
             ans = max(ans,1+help(arr,d,j));
+        
         //left
         for(int j = i-1 ; j >= max(i-d,0) && arr[i] > arr[j] ; j--)
             ans = max(ans,1+help(arr,d,j));
+        
         return dp[i]=ans;
     }
     int maxJumps(vector<int>& arr, int d) {
