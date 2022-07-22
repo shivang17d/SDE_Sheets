6080c5bf7c5e1b20b9f5fab336321ed98567e301
Shivang Gupta <shivangguptaxd@gmail.com>
2022-07-22 23:05:29 +0530

    initial commit

--- Typical Questions/1.4 ) Subsets/1 (Subsets).cpp
+++ Typical Questions/1.4 ) Subsets/1 (Subsets).cpp
@@ -1,3 +1,5 @@
+//Method 1
+
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
@@ -11,6 +13,35 @@ public:
             }   
             ans.push_back(v);
         }
+        return ans;
+    }
+};
+
+
+
+//Method 2
+
+class Solution {
+public:
+    void help(int i, vector<int>& nums,vector<int>& temp,vector<vector<int>>& ans) {
+        if(i == nums.size()) {
+            ans.push_back(temp);
+        }
+        else {
+            // to take the ith element
+            temp.push_back(nums[i]);
+            help(i+1,nums,temp,ans);
+            
+            temp.pop_back();
+            
+            //to ignore the ith element
+            help(i+1,nums,temp,ans);
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        help(0,nums,temp,ans);
         return ans;
     }
 };
